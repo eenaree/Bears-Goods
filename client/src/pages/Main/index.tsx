cd357d5465cb7f21b9dd353392fb51cd35eb870d
eenaree <narilee91@hanmail.net>
2022-05-28 17:53:26 +0900

    Delete: GoodsFilter component - 페이지 라우팅을 이용한 카테고리 필터기능으로 변경할 예정

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import goodsAPI from '@api/goods';
 import GoodsCard from '@components/GoodsCard';
-import GoodsFilter from '@components/GoodsFilter';
 import { GoodsData } from '@typings/db';
 import { GoodsCardList } from './styles';
 
@@ -23,30 +22,10 @@ export default function Main(): React.ReactElement {
       .catch(error => console.error(error));
   }, []);
 
-  const [selectedFilter, setSelectedFilter] = useState<string>('all');
-  const onChangeFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedFilter(e.target.defaultValue);
-  };
-  const [filteredGoodsList, setFilteredGoodsList] = useState<GoodsData[]>([]);
-
-  useEffect(() => {
-    if (selectedFilter !== 'all') {
-      const filteredGoodsList = goodsList.filter(
-        goods => goods.category === selectedFilter
-      );
-      setFilteredGoodsList(filteredGoodsList);
-    }
-  }, [selectedFilter, goodsList]);
-
   return (
     <>
-      <GoodsFilter filter={selectedFilter} onChangeFilter={onChangeFilter} />
       {goodsList.length > 0 && (
-        <GoodsCardList>
-          {selectedFilter === 'all'
-            ? renderGoodsCardList(goodsList)
-            : renderGoodsCardList(filteredGoodsList)}
-        </GoodsCardList>
+        <GoodsCardList>{renderGoodsCardList(goodsList)}</GoodsCardList>
       )}
     </>
   );
