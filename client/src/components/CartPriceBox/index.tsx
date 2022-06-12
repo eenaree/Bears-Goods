ba9d047af91cbae5b1329f8a4293ff89f2003520
eenaree <narilee91@hanmail.net>
2022-06-12 16:15:25 +0900

    Feat: 추가 CartPriceBox component
    장바구니 페이지에서 로직이 직관적으로 보일 수 있도록 별도 컴포넌트로 생성.
    부모 컴포넌트에서 전달하는 props 값에 의존하여 상태값 계산을 메모이제이션하지만,
    props 값이 워낙 변화가 심해 useMemo를 사용한 이점은 크지 않은 것 같다.

---
+++ client/src/components/CartPriceBox/index.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { useMemo } from 'react';
+import { FcAbout } from 'react-icons/fc';
+import { addThousandSeperatorToNumber } from '@utils';
+import { CartItemOption } from '@typings/db';
+import { PriceBox, ToolTipText } from './styles';
+
+interface Props {
+  selectedCart: CartItemOption[];
+}
+
+export default function CartPriceBox({
+  selectedCart,
+}: Props): React.ReactElement {
+  const selectedCartTotalPrice = useMemo(
+    () =>
+      selectedCart.reduce((prev, curr) => prev + curr.price * curr.quantity, 0),
+    [selectedCart]
+  );
+
+  const deliveryFee = useMemo(
+    () =>
+      selectedCart.length > 0 && selectedCartTotalPrice < 50000 ? 3000 : 0,
+    [selectedCart, selectedCartTotalPrice]
+  );
+
+  return (
+    <PriceBox>
+      <p>
+        <span>주문금액</span>
+        <span>{addThousandSeperatorToNumber(selectedCartTotalPrice)} 원</span>
+      </p>
+      <p>
+        <span>
+          배송비 <FcAbout />
+          <ToolTipText>5만원 이상 구매시 무료배송</ToolTipText>
+        </span>
+        <span>{addThousandSeperatorToNumber(deliveryFee)} 원</span>
+      </p>
+      <hr />
+      <p>
+        <span>총 주문금액</span>
+        <span>
+          <strong>
+            {addThousandSeperatorToNumber(selectedCartTotalPrice + deliveryFee)}
+          </strong>
+          &nbsp;원
+        </span>
+      </p>
+    </PriceBox>
+  );
+}
--- client/src/pages/Cart/index.tsx
+++ client/src/pages/Cart/index.tsx
@@ -1,22 +1,19 @@
 import * as React from 'react';
 import { useEffect, useState, useMemo } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useCartDispatch, useCartState } from '@context/CartContext';
 import CartItem from '@components/CartItem';
-import { addThousandSeperatorToNumber } from '@utils';
 import {
   CartItemList,
   Container,
-  PriceBox,
   ButtonGroup,
   SelectZone,
   NoneCartList,
-  ToolTipText,
 } from './styles';
 import DeleteConfirmModal from '@components/DeleteConfirmModal';
 import { CartItemOption } from '@typings/db';
-import { FcAbout } from 'react-icons/fc';
 import CheckBox from '@components/CheckBox';
+import CartPriceBox from '@components/CartPriceBox';
 
 export default function Cart(): React.ReactElement {
   const { cart } = useCartState();
@@ -33,14 +30,8 @@ export default function Cart(): React.ReactElement {
     () => cart.filter(item => item.selected),
     [cart]
   );
-  const selectedCartItemPrice = useMemo(
-    () =>
-      selectedCartItem.reduce(
-        (prev, curr) => prev + curr.quantity * curr.price,
-        0
-      ),
-    [selectedCartItem]
-  );
+
+  const [modal, setModal] = useState<boolean>(false);
   const handleDeleteClick = () => {
     if (!selectedCart.length) {
       alert('선택된 상품이 없습니다. 삭제할 상품을 선택하세요.');
@@ -73,45 +64,9 @@ export default function Cart(): React.ReactElement {
               <button onClick={handleDeleteClick}>선택 삭제</button>
             </SelectZone>
             <CartItemList>{renderCartItemList}</CartItemList>
-            <PriceBox>
-              <p>
-                <span>주문금액</span>
-                <span>
-                  {addThousandSeperatorToNumber(selectedCartItemPrice)}
-                  &nbsp;원
-                </span>
-              </p>
-              <p>
-                <span>
-                  배송비
-                  <FcAbout />
-                  <ToolTipText>5만원 이상 구매시 무료배송</ToolTipText>
-                </span>
-                <span>
-                  {selectedCartItem.length > 0 && selectedCartItemPrice < 50000
-                    ? addThousandSeperatorToNumber(3000)
-                    : 0}
-                  &nbsp;원
-                </span>
-              </p>
-              <hr />
-              <p>
-                <span>총 주문금액</span>
-                <span>
-                  <strong>
-                    {selectedCartItem.length > 0 &&
-                    selectedCartItemPrice < 50000
-                      ? addThousandSeperatorToNumber(
-                          selectedCartItemPrice + 3000
-                        )
-                      : addThousandSeperatorToNumber(selectedCartItemPrice)}
-                  </strong>
-                  &nbsp;원
-                </span>
-              </p>
-            </PriceBox>
+            <CartPriceBox selectedCart={selectedCart} />
             <ButtonGroup>
-              <button onClick={handleListNavigate}>목록으로</button>
+              <Link to="/">목록으로</Link>
               <button>주문하기</button>
             </ButtonGroup>
           </>
