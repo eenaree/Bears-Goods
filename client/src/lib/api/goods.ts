e9bb57648fbd1b4f83ccd603ea635ec3e4aa32c0
eenaree <narilee91@hanmail.net>
2022-07-06 02:01:33 +0900

    Remove: 카테고리 관련 코드 삭제

--- client/src/lib/api/goods.ts
+++ client/src/lib/api/goods.ts
@@ -3,16 +3,9 @@ import { server } from '@api/default';
 import { GoodsData } from '@typings/db';
 
 const goodsAPI = {
-  getGoodsList: (
-    config: AxiosRequestConfig,
-    category?: string | null,
-    sort?: string | null,
-    order?: string | null
-  ) =>
+  getGoodsList: (config: AxiosRequestConfig, category?: string) =>
     server.get<GoodsData[]>(
-      `/goods?${category ? `category=${category}` : ''}${
-        sort ? `&_sort=${sort}` : ''
-      }${order ? `&_order=${order}` : ''}`,
+      `/goods?${category ? `category=${category}` : ''}`,
       config
     ),
   getGoods: (id: string) => server.get<GoodsData>(`/goods/${id}`),
--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,21 +1,12 @@
 import * as React from 'react';
-import { useCallback } from 'react';
 import { BiError } from 'react-icons/bi';
 import { useSearchParams } from 'react-router-dom';
 import goodsAPI from '@api/goods';
-import Category from '@components/Category';
 import GoodsCard from '@components/GoodsCard';
-import PriceSorting from '@components/PriceSorting';
 import ProgressBar from '@components/ProgressBar';
 import useAxiosWithAbort from '@hooks/useAxiosWithAbort';
 import { GoodsData } from '@typings/db';
 import { CreateError, GoodsCardList } from './styles';
-
-interface GoodsFilter {
-  category: string;
-  sort: string;
-  order: string;
-}
 
 const renderGoodsCardList = (goodsList: GoodsData[]): React.ReactElement[] =>
   goodsList.map(goods => (
@@ -29,43 +20,17 @@ const renderGoodsCardList = (goodsList: GoodsData[]): React.ReactElement[] =>
   ));
 
 export default function Main(): React.ReactElement {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const { category, sort, order }: GoodsFilter = {
-    category: searchParams.get('category') || '',
-    order: searchParams.get('order') || '',
-    sort: searchParams.get('order') ? 'price' : '',
-  };
-  const handleChangeFormFilter = useCallback(
-    (e: React.ChangeEvent<HTMLFormElement>) => {
-      const formData = new FormData(e.currentTarget);
-      const params = new URLSearchParams();
-      for (const [key, value] of formData) {
-        if (value !== '') {
-          if (key === 'order') {
-            params.set('sort', 'price');
-          }
-          params.set(key, value as string);
-        }
-      }
-      setSearchParams(params);
-    },
-    []
-  );
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get('category') || '';
 
   const { status, data, error } = useAxiosWithAbort<GoodsData[]>(
     goodsAPI.getGoodsList,
-    category,
-    sort,
-    order
+    category
   );
 
   return (
     <>
       <ProgressBar isLoading={status === 'loading'} />
-      <form onChange={handleChangeFormFilter}>
-        <Category category={category} />
-        <PriceSorting order={order} />
-      </form>
       {data && <GoodsCardList>{renderGoodsCardList(data)}</GoodsCardList>}
       {error && (
         <CreateError>
