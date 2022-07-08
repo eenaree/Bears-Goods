d2b1e079f662d3f387a115a5dbc7fb31830bf5a5
eenaree <narilee91@hanmail.net>
2022-07-09 01:21:51 +0900

    Correct: getGoodsList api 호출시, 잘못된 방식으로 쿼리 전달한 것 수정

--- client/src/lib/api/goods.ts
+++ client/src/lib/api/goods.ts
@@ -2,9 +2,12 @@ import { goodsAPI } from '@api/default';
 import { GoodsData } from '@typings/db';
 
 export default {
-  getGoodsList: (signal: AbortSignal, category?: string) => {
+  getGoodsList: (signal: AbortSignal, category: string) => {
     return goodsAPI.request({
-      url: `/goods?${category ? `category=${category}` : ''}`,
+      url: `/goods`,
+      params: {
+        category: category || null,
+      },
       signal,
     });
   },
