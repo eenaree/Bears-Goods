06434fcede22341980182d90a453e20c824c3bd2
eenaree <narilee91@hanmail.net>
2022-07-16 16:21:54 +0900

    Feat: 선택한 옵션 수량 변경 기능 구현
    사용자가 기본값을 지우고 추가 입력을 하지 않는 경우에 0으로 표시되기 때문에,
    0으로 저장되는 것을 막기 위해, onBlur 메서드 안에서 추가 조정 과정을 거치는 코드를 추가함.
    
    조정 과정이 있기 때문에, 실제 저장하고 있는 옵션에 변경된 값을 반영하는 이벤트도 이때 실행하도록 함.

---
+++ client/src/components/GoodsItem/GoodsOptionQuantityInput.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { useState } from 'react';
+import QuantityInput from '@components/QuantityInput';
+import { useOptionDispatch } from '@context/OptionContext';
+import { GoodsOption } from '@typings/db';
+
+interface Props {
+  size: GoodsOption['size'];
+  quantity: GoodsOption['quantity'];
+}
+
+export default function GoodsOptionQuantityInput({
+  size,
+  quantity,
+}: Props): React.ReactElement {
+  const optionDispatch = useOptionDispatch();
+  const [value, setValue] = useState<number>(quantity);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value);
+    if (Number.isNaN(value)) return;
+    setValue(value);
+  };
+
+  const onBlur = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === '0') {
+      alert('최소 구매 수량은 1개입니다.');
+      setValue(quantity);
+      return;
+    }
+    optionDispatch({ type: 'CHANGE_OPTION_QUANTITY', size, quantity: value });
+  };
+
+  const incrementValue = () => {
+    if (value === 20) {
+      alert('최대 구매 가능 수량은 20개입니다.');
+      return;
+    }
+    setValue(prev => prev + 1);
+    optionDispatch({ type: 'INCREMENT_OPTION_QUANTITY', size });
+  };
+
+  const decrementValue = () => {
+    if (value === 1) {
+      alert('최소 구매 수량은 1개 입니다.');
+      return;
+    }
+    setValue(prev => prev - 1);
+    optionDispatch({ type: 'DECREMENT_OPTION_QUANTITY', size });
+  };
+
+  return (
+    <QuantityInput
+      value={value}
+      onChange={onChange}
+      onBlur={onBlur}
+      onIncrement={incrementValue}
+      onDecrement={decrementValue}
+    />
+  );
+}
