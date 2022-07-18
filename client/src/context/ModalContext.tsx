54c8068cabc4da63f93a50a7582ef049de5fcb61
eenaree <narilee91@hanmail.net>
2022-07-18 21:23:41 +0900

    Add: Modal Context 생성 #6
    모달마다 모달 상태값을 생성하는 코드를 줄이고,
    모달과 관련된 코드 범위를 좁히기 위해 생성함.

---
+++ client/src/context/ModalContext.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { createContext, useContext, useReducer } from 'react';
+
+interface Props {
+  children: React.ReactElement | React.ReactElement[];
+}
+
+type ModalActions = { type: 'OPEN_MODAL' } | { type: 'CLOSE_MODAL' };
+
+const modalReducer = (state: boolean, action: ModalActions): boolean => {
+  switch (action.type) {
+    case 'OPEN_MODAL':
+      return true;
+    case 'CLOSE_MODAL':
+      return false;
+  }
+};
+
+const ModalContext = createContext<boolean | undefined>(undefined);
+
+const ModalDispatchContext = createContext<
+  React.Dispatch<ModalActions> | undefined
+>(undefined);
+
+export default function ModalProvider({ children }: Props): React.ReactElement {
+  const [modal, dispatch] = useReducer(modalReducer, false);
+
+  return (
+    <ModalDispatchContext.Provider value={dispatch}>
+      <ModalContext.Provider value={modal}>{children}</ModalContext.Provider>
+    </ModalDispatchContext.Provider>
+  );
+}
+
+export const useModal = () => {
+  const state = useContext(ModalContext);
+  if (state === undefined) {
+    throw new Error('ModalContextProvider can not found');
+  }
+  return state;
+};
+
+export const useModalDispatch = () => {
+  const dispatch = useContext(ModalDispatchContext);
+  if (dispatch === undefined) {
+    throw new Error('ModalDispatchContextProvider can not found');
+  }
+  return dispatch;
+};
