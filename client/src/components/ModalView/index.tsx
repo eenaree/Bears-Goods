7aa3e68c72bad654f2aaace7c5e96a9033ee3bfd
eenaree <narilee91@hanmail.net>
2022-07-18 21:34:20 +0900

    Add: 모달 언마운트시 애니메이션 실행 코드 추가 #6
    - modal: 모달 컨텍스트에 정의한 상태값은 모달을 단순 트리거하는 용도.
    모달 컴포넌트가 언마운트될 때 애니메이션을 실행시키기 위한 상태값.
    - isMounted: 모달 컴포넌트 렌더링 여부를 결정함.
    
    언마운트될 때, fadeOut 애니메이션 실행 시간과 setTimeout 타이머를 똑같이 설정하게 되면,
    화면이 한번 빠르게 깜빡거리며 모달이 사라지는 문제가 발생함.
    애니메이션이 종료되는 시점과 타이머 콜백함수의 실행시점이 정확하게 맞물리지 않기 때문에,
    애니메이션 실행 시간을 콜백함수가 실행되는 시점보다 조금 더 길게 설정함.

--- client/src/components/ModalView/index.tsx
+++ client/src/components/ModalView/index.tsx
@@ -1,12 +1,35 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useModal } from '@context/ModalContext';
 import { styles } from './styles';
 
 interface Props {
   children: React.ReactChild | React.ReactChild[];
 }
 
-export default function ModalView({ children }: Props): React.ReactElement {
+export default function ModalView({
+  children,
+}: Props): React.ReactElement | null {
+  const modal = useModal();
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    let timeoutId: number;
+
+    if (modal && !isMounted) {
+      setIsMounted(true);
+    }
+    if (!modal && isMounted) {
+      timeoutId = window.setTimeout(() => {
+        setIsMounted(false);
+      }, 500);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [modal, isMounted]);
+
   useEffect(() => {
     document.body.style.overflow = 'hidden';
     return () => {
@@ -14,9 +37,11 @@ export default function ModalView({ children }: Props): React.ReactElement {
     };
   }, []);
 
+  if (!isMounted) return null;
+
   return (
-    <div css={styles.modalContainer}>
-      <div css={styles.modalView}>{children}</div>
+    <div css={styles.modalContainer(modal)}>
+      <div css={styles.modalView(modal)}>{children}</div>
     </div>
   );
 }
