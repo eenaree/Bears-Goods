12c9618d7079b8a6b849ae3c1d1d07f46fdccc4e
eenaree <narilee91@hanmail.net>
2022-07-18 21:34:20 +0900

    Add: 모달 언마운트시 애니메이션 실행 코드 추가 #6
    - modal: 모달 컨텍스트에 정의한 상태값은 모달을 단순 트리거하는 용도.
    모달 컴포넌트가 언마운트될 때 애니메이션을 실행시키기 위한 상태값.
    - isMounted: 모달 컴포넌트 렌더링 여부를 결정함.
    
    언마운트될 때, fadeOut 애니메이션 실행 시간과 setTimeout 타이머를 똑같이 설정하게 되면,
    화면이 한번 빠르게 깜빡거리며 모달이 사라지는 문제가 발생함.
    애니메이션이 종료되는 시점과 타이머 콜백함수의 실행시점이 정확하게 맞물리지 않기 때문에,
    애니메이션 실행 시간을 콜백함수가 실행되는 시점보다 조금 더 길게 설정함.

--- client/src/components/ModalView/styles.tsx
+++ client/src/components/ModalView/styles.tsx
@@ -1,28 +1,54 @@
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
 import { mq } from '@styles/mediaQueries';
 
+const modalFadeIn = keyframes({
+  from: { opacity: 0 },
+  to: { opacity: 1 },
+});
+
+const modalFadeOut = keyframes({
+  from: { opacity: 1 },
+  to: { opacity: 0 },
+});
+
+const modalViewSlideDown = keyframes({
+  from: { transform: 'translateY(-50px)' },
+  to: { transform: 'translateY(0)' },
+});
+
+const modalViewSlideUp = keyframes({
+  from: { transform: 'translateY(0)' },
+  to: { transform: 'translateY(-50px)' },
+});
+
 export const styles = {
-  modalContainer: css({
-    zIndex: 1,
-    position: 'fixed',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }),
-  modalView: css({
-    width: 200,
-    padding: 20,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    boxShadow: '0 1px 4px rgba(255,255,255,0.4)',
-    fontSize: '1.4rem',
-    [mq('sm')]: {
-      width: 300,
-    },
-  }),
+  modalContainer: (modal: boolean) =>
+    css({
+      zIndex: 1,
+      position: 'fixed',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: 'rgba(0,0,0,0.5)',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      animationName: modal ? modalFadeIn : modalFadeOut,
+      animationDuration: '0.6s',
+    }),
+  modalView: (modal: boolean) =>
+    css({
+      width: 200,
+      padding: 20,
+      backgroundColor: '#fff',
+      borderRadius: 5,
+      boxShadow: '0 1px 4px rgba(255,255,255,0.4)',
+      fontSize: '1.4rem',
+      animationName: modal ? modalViewSlideDown : modalViewSlideUp,
+      animationDuration: '0.6s',
+      [mq('sm')]: {
+        width: 300,
+      },
+    }),
 };
