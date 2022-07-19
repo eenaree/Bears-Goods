7d51d7224c6a50445b01818f2f0e9ec9c1b3a531
eenaree <narilee91@hanmail.net>
2022-07-19 23:12:04 +0900

    Remove: CartContext 삭제하면서 영향 받는 코드 모두 삭제 #12

--- client/src/client.tsx
+++ client/src/client.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from '@context/CartContext';
 import App from '@layouts/App';
 import GlobalStyles from '@styles/Global';
 
 ReactDOM.render(
   <BrowserRouter>
     <GlobalStyles />
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
