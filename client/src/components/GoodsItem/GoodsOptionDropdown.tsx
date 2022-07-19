ddbd04807d4d0b58a0c41346c3b740cbd90644a9
eenaree <narilee91@hanmail.net>
2022-07-19 21:21:22 +0900

    Feat: 선택한 옵션 중복성 체크하기 기능 구현 #10
    ref 객체에 담아 렌더링과 상관없이 값을 변경하도록 함.
    선택한 옵션을 삭제하는 기능이 있기 때문에, 부모 컴포넌트에서 props로 전달함.

--- client/src/components/GoodsItem/GoodsOptionDropdown.tsx
+++ client/src/components/GoodsItem/GoodsOptionDropdown.tsx
@@ -5,10 +5,12 @@ import { GoodsData, GoodsOption } from '@typings/db';
 
 interface Props {
   item: GoodsData;
+  selectedRef: React.MutableRefObject<Set<string | number>>;
 }
 
 export default function GoodsOptionDropdown({
   item,
+  selectedRef,
 }: Props): React.ReactElement {
   const optionDispatch = useOptionDispatch();
   const sizes = item.size.map(size => ({
@@ -21,6 +23,12 @@ export default function GoodsOptionDropdown({
       ? size
       : Number(size);
 
+    if (selectedRef.current.has(parsedSize)) {
+      alert('이미 선택한 옵션입니다.');
+      return;
+    }
+
+    selectedRef.current.add(parsedSize);
     const newOption = { ...item, size: parsedSize, quantity: 1 };
     optionDispatch({ type: 'ADD_OPTION', option: newOption });
   };
--- client/src/components/GoodsItem/GoodsOptionPicker.tsx
+++ client/src/components/GoodsItem/GoodsOptionPicker.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useRef } from 'react';
 import { useOption } from '@context/OptionContext';
 import { GoodsData } from '@typings/db';
 import GoodsOptionDropdown from './GoodsOptionDropdown';
@@ -11,13 +12,18 @@ interface Props {
 
 export default function GoodsOptionPicker({ item }: Props): React.ReactElement {
   const option = useOption();
+  const selectedRef = useRef(new Set(option.map(option => option.size)));
 
   return (
     <div css={styles.optionArea}>
-      <GoodsOptionDropdown item={item} />
+      <GoodsOptionDropdown item={item} selectedRef={selectedRef} />
       <div>
         {option.map(option => (
-          <SelectedOption key={option.size} {...option} />
+          <SelectedOption
+            key={option.size}
+            selectedRef={selectedRef}
+            {...option}
+          />
         ))}
       </div>
     </div>
