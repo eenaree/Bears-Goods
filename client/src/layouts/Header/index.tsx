0f3ede1563ba1735b217f06e918e8ed25f80e3fb
eenaree <narilee91@hanmail.net>
2022-07-19 23:12:04 +0900

    Remove: CartContext 삭제하면서 영향 받는 코드 모두 삭제 #12

--- client/src/layouts/Header/index.tsx
+++ client/src/layouts/Header/index.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
-import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { Link, useNavigate } from 'react-router-dom';
-import { useCartListCount } from '@context/CartContext';
 import { styles } from './styles';
 
 export default function Header(): React.ReactElement {
-  const cartListCount = useCartListCount();
   const navigate = useNavigate();
   const handleCartCheck = () => {
     navigate('/goods_cart');
@@ -17,12 +14,6 @@ export default function Header(): React.ReactElement {
         <h1 css={styles.headerLogo}>
           <Link to="/">Bears Goods</Link>
         </h1>
-        <button css={styles.cartButton} onClick={handleCartCheck}>
-          <AiOutlineShoppingCart size="2.8rem" />
-          {cartListCount > 0 && (
-            <span css={styles.cartItemCount}>{cartListCount}</span>
-          )}
-        </button>
       </div>
     </header>
   );
