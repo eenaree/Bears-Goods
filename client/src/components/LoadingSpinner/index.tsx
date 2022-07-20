4264459f2ec3bfed585e997da5b9ce77f7f69386
Nari Lee <narilee91@hanmail.net>
2022-07-20 15:34:06 +0900

    Feat: loading spinner 컴포넌트 생성 (#16)
    * Add: css animation으로 spinner 무한 회전 형태 생성 #14
    
    * Add: axios Interceptor 로딩 상태 연결 #14

---
+++ client/src/components/LoadingSpinner/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { css, keyframes } from '@emotion/react';
+import { useLoader } from '@context/LoaderContext';
+
+const infiniteLotation = keyframes({
+  from: { transform: 'rotate(0deg)' },
+  to: { transform: 'rotate(360deg)' },
+});
+
+const styles = {
+  spinner: (isLoading: boolean) =>
+    isLoading &&
+    css({
+      zIndex: 1,
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      marginLeft: '-25px',
+      marginTop: '-25px',
+      width: 40,
+      height: 40,
+      border: '5px solid #eee',
+      borderTopColor: '#666',
+      borderRadius: '50%',
+      animationName: `${infiniteLotation}`,
+      animationDuration: '1s',
+      animationIterationCount: 'infinite',
+      animationTimingFunction: 'linear',
+    }),
+};
+
+export default function LoadingSpinner(): React.ReactElement {
+  const isLoading = useLoader();
+  return <div css={styles.spinner(isLoading)} />;
+}
