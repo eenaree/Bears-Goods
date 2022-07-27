3901086044d7b968072a612de2a589fd372379e0
Nari Lee <narilee91@hanmail.net>
2022-07-28 00:57:33 +0900

    Add: 상세페이지 에러 처리 화면 렌더링 추가 (#30)
    * Add: 에러 상태값 추가 및 에러 메세지 전달 #29
    
    * Add: 언마운트시 setState 실행 방지 코드 추가 #29
    
    데이터를 모두 가져오기 전에, 다른 페이지로 이동할 때
    불필요한 setState가 발생하지 않도록 하기 위해 추가함.

--- client/src/pages/Detail/index.tsx
+++ client/src/pages/Detail/index.tsx
@@ -2,29 +2,51 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import goodsAPI from '@api/goods';
+import ErrorMessage from '@components/ErrorMessage';
 import GoodsItem from '@components/GoodsItem';
 import LoadingSpinner from '@components/LoadingSpinner';
 import { GoodsData } from '@typings/db';
 
+interface Payload {
+  goods: GoodsData | null;
+  error: string | null;
+}
+
 export default function Detail() {
   const params = useParams<'id'>();
-  const [goods, setGoods] = useState<GoodsData | null>(null);
+  const [{ goods, error }, setPayload] = useState<Payload>({
+    goods: null,
+    error: null,
+  });
 
   useEffect(() => {
+    let shouldSetState = true;
+
     if (params.id) {
       goodsAPI
         .getGoods(params.id)
         .then(({ data }) => {
-          setGoods(data);
+          if (!shouldSetState) return;
+          setPayload({ goods: data, error: null });
         })
-        .catch(error => console.error(error));
+        .catch((error: unknown) => {
+          if (!shouldSetState) return;
+          if (error instanceof Error) {
+            setPayload({ goods: null, error: error.message });
+          }
+        });
     }
+
+    return () => {
+      shouldSetState = false;
+    };
   }, [params.id]);
 
   return (
     <>
       <LoadingSpinner />
       <main>{goods && <GoodsItem item={goods} />}</main>
+      {error && <ErrorMessage error={error} />}
     </>
   );
 }
