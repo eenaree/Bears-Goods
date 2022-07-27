02c00bd773aa9170577c81d43373b13b0d409e80
eenaree <narilee91@hanmail.net>
2022-07-28 01:17:23 +0900

    Add: 404 페이지 추가

--- client/src/layouts/App/index.tsx
+++ client/src/layouts/App/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import ErrorMessage from '@components/ErrorMessage';
 import Cart from '@pages/Cart';
 import Detail from '@pages/Detail';
 import Main from '@pages/Main';
@@ -12,6 +13,10 @@ export default function App() {
         <Route path="/" element={<Main />} />
         <Route path="/goods/:id" element={<Detail />} />
         <Route path="/goods_cart" element={<Cart />} />
+        <Route
+          path="*"
+          element={<ErrorMessage error="요청한 페이지를 찾을 수 없습니다." />}
+        />
       </Route>
     </Routes>
   );
