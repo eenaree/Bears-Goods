779b4eb0aed2549527fff07574f615cecbbdd9a1
eenaree <narilee91@hanmail.net>
2022-08-20 00:07:00 +0900

    Refactor: media query 함수 매개변수 타입 명확하게 지정
    매개변수의 타입을 breakpoints 객체의 키 값으로 정확하게 설정하여,
    원하지 않는 breakpoint가 입력되었을 때, 타입 오류가 발생하도록 함.
    
    => 그 과정에서 Object.keys 메서드의 리턴 타입이 string으로 고정되어 있는 문제가 있어
    새로운 변수에 이 메서드 실행값을 담아 타입 단언을 하는 과정을 추가함.
    breakpoints 객체의 프로퍼티를 모두 알고 있고 추가되지 않는다는 것을 알고 있기 때문에,
    타입 단언이 문제가 되지는 않지만 추후 개선이 필요한 코드라고 생각됨.

--- client/src/styles/mediaQueries.ts
+++ client/src/styles/mediaQueries.ts
@@ -1,16 +1,17 @@
-interface BreakPoints {
-  [point: string]: number;
-}
-
-const breakpoints: BreakPoints = {
+const breakpoints = {
   xs: 576,
   sm: 768,
   md: 992,
   lg: 1200,
-};
+} as const;
 
-export const mq = (point: string) => {
-  const bpArray = Object.keys(breakpoints).map<[string, number]>(key => [
+type Points = keyof typeof breakpoints;
+type PointValues = typeof breakpoints[Points];
+
+const getObjKeys = Object.keys as <T extends object>(obj: T) => Array<keyof T>;
+
+export function mq(point: Points) {
+  const bpArray = getObjKeys(breakpoints).map<[Points, PointValues]>(key => [
     key,
     breakpoints[key],
   ]);
@@ -18,9 +19,10 @@ export const mq = (point: string) => {
   const [result] = bpArray.reduce<string[]>((acc, [name, size]) => {
     if (name === point) {
       return [...acc, `@media (min-width: ${size}px)`];
+    } else {
+      return acc;
     }
-    return acc;
   }, []);
 
   return result;
-};
+}
